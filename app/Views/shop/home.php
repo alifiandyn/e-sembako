4cf168eeed2d70e5e2beb919c5a9def0b28d61fb
Alifiandy Nugraha <alifiandyn@gmail.com>
2022-06-06 01:42:20 +0700

    add modal notification on home page

--- app/Views/shop/home.php
+++ app/Views/shop/home.php
@@ -66,6 +66,29 @@
 </div>
 <!-- Products End -->
 
+<!-- Modal Notification -->
+<?php if (session()->getFlashdata('message')) : ?>
+    <div class="modal fade" id="modalNotification" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Notifikasi</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <?= session()->getFlashdata('message');  ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+<?php endif; ?>
+<!-- Modal Notification -->
 
 <?= $this->include('shop/layout/footer'); ?>
 <?= $this->endSection(); ?>
